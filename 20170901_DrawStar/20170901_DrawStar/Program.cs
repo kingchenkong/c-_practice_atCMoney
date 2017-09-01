14131a5480e4821c74b70d70ad2f864e98028580
陳維漢 <wikeyno@gmail.com>
2017-09-01 18:03:25 +0800

    homework is done.

---
+++ 20170901_DrawStar/20170901_DrawStar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _DrawStar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("size:");
+                int size = Int32.Parse(Console.ReadLine());
+                if (size == -1)
+                    break;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size - i - 1; j++)
+                        Console.Write(" ");
+                    Console.Write("*");
+                    for (int k = size - i; k < size; k++)
+                        Console.Write("**");
+                    Console.WriteLine();
+                }
+                for (int i = 0; i < size - 1; i++)
+                {
+                    for (int j = 0; j < i + 1; j++)
+                        Console.Write(" ");
+                    Console.Write("*");
+                    for (int k = 1; k < size - i - 1; k++)
+                        Console.Write("**");
+                    Console.WriteLine();
+                }
+            } while (true);
+        }
+    }
+}
---
+++ 20170901_Practice_Prim/20170901_Practice_Prim/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _Practice_Prim
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("請輸入 正整數");
+                int i1 = Int32.Parse(Console.ReadLine());
+                if (i1 == -1)
+                {
+                    Console.WriteLine("Quit.");
+                    return;
+                }
+                else if (i1 < 2)
+                {
+                    Console.WriteLine("not Prime.");
+                }
+                else if (i1 == 2)
+                {
+                    Console.WriteLine("is Prime.");
+                }
+                else
+                {
+                    process(i1);
+                }
+            } while (true);
+        }
+        public static void process(int int1)
+        {
+            //int count = 0;
+            for (int i = 2; i <= int1 / 2; i++)
+            {
+                if (int1 % i == 0)
+                {
+                    Console.WriteLine("not Prime.");
+                    return;
+                }
+            }
+            Console.WriteLine("is Prime.");
+        }
+    }
+}
