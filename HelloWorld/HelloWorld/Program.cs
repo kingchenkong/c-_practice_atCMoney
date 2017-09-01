c6b9d02501d302f7beff470a5df541d4fa6e7b9e
陳維漢 <wikeyno@gmail.com>
2017-09-01 16:57:21 +0800

    prac1 is finish

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,17 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
+            //Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            string s1 = Console.ReadLine();
+            int a1 = Int32.Parse(s1);
+            string s2 = Console.ReadLine();
+            double a2 = Double.Parse(s2);
+            int sum = a1 + (int)a2;
+            Console.WriteLine(sum);
+
         }
     }
 }
---
+++ calculate/calculate/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace calculate
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("四則運算 Part II");
+
+            do
+            {
+                Console.WriteLine("運算: 1) +, 2) -, 3)*, 4) /, -1)Quit:");
+                int option = Int32.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case -1:
+                        Console.WriteLine("Quit.");
+                        return ;
+                    case 1:
+                        CalAdd(inputFunc("1"), inputFunc("2"));
+                        break;
+                    case 2:
+                        CalSub(inputFunc("1"), inputFunc("2"));
+                        break;
+                    case 3:
+                        CalMul(inputFunc("1"), inputFunc("2"));
+                        break;
+                    case 4:
+                        CalDiv(inputFunc("1"), inputFunc("2"));
+                        break;
+                    default:
+                        Console.WriteLine("Input Error.");
+                        break;
+                }
+            } while (true);
+        }
+        public static int inputFunc(String str)
+        {
+            Console.WriteLine("integer " + str + ":");
+            return Int32.Parse(Console.ReadLine());
+        }
+        public static void CalAdd(int a, int b)
+        {
+            Console.WriteLine("Answer: " + (a + b));
+        }
+        public static void CalSub(int a, int b)
+        {
+            Console.WriteLine("Answer: " + (a - b));
+        }
+        public static void CalMul(int a, int b)
+        {
+            Console.WriteLine("Answer: " + (a * b));
+        }
+        public static void CalDiv(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Input Error.");
+                return;
+            }
+
+            Console.WriteLine("Answer: " + (a / b));
+        }
+    }
+}
