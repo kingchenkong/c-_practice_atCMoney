c344dee703df1ba7675b6496bfb7735e310a884f
陳維漢 <wikeyno@gmail.com>
2017-09-05 16:49:35 +0800

    20170905 C# file manager is done.

---
+++ 20170905_CSharp_FileManager/20170905_CSharp_FileManager/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+
+namespace _CSharp_FileManager
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //test();
+
+            // begin
+            //Trader trader = new Trader("TestFile.csv");
+            Trader trader = new Trader("20110315.csv");
+            do
+            {
+                Console.WriteLine("input Stock Code:");
+                string scan = Console.ReadLine();
+                Console.WriteLine("input output File Name:");
+                string scanFileName = Console.ReadLine();
+                string[] arrSplit = scanFileName.Split('.');
+
+                if (arrSplit.Length == 1)
+                {
+                        scanFileName += ".csv";
+                }
+				else if (arrSplit.Length > 2 || arrSplit.Length == 0)
+				{
+					Console.WriteLine("Error: File Name illegal.");
+                    continue;
+				}
+                    trader.SetDestinationFileName(scanFileName);
+                    if (trader.SearchStock(scan, scanFileName))
+                        Console.WriteLine("is done.");
+                    else
+                        Console.WriteLine("Fail.");
+            } while (true);
+        }
+        public static void test()
+        {
+            Trader trader = new Trader("TestFile.csv");
+
+            do
+            {
+                Console.WriteLine("input index:");
+                string scan = Console.ReadLine();
+                try
+                {
+                    int index = Int32.Parse(scan);
+                    if (index == -1)
+                    {
+                        break;
+                    }
+                    if (index < -1)
+                    {
+                        Console.WriteLine("Error: index < 0");
+                        continue;
+                    }
+
+                    string printStr = trader.GetStockDataByIndex(index);
+                    if (printStr == null)
+                    {
+                        Console.WriteLine("Error: index is not exist.");
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(printStr);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Error: Input Format.");
+                }
+            } while (true);
+
+
+
+
+            // get Current Directory
+            //Console.WriteLine(System.Environment.CurrentDirectory);
+
+            //File Manager
+            //string sourceFile = "TestFile.csv";
+            //string targetFile = "target.csv";
+
+            //Stock[] arrStock = new Stock[100];
+            //string title;
+
+            //try
+            //{
+            //    // in
+            //    FileStream fromStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+            //    StreamReader fileReader = new StreamReader(fromStream);
+            //    // out
+            //    FileStream toStream = new FileStream(targetFile, FileMode.Create, FileAccess.Write);
+            //    StreamWriter fileWriter = new StreamWriter(toStream);
+
+            //    String loadInStr;
+            //    int dataCount = 0;
+            //    title = fileReader.ReadLine();
+
+            //    while ((loadInStr = fileReader.ReadLine()) != null)
+            //    {
+            //        //Console.WriteLine(loadInStr);
+            //        //fileWriter.WriteLine(loadInStr);
+            //        string[] arrToSplit = loadInStr.Split(',');
+
+            //        if (dataCount == arrStock.Length)
+            //            arrStock = Resize(arrStock);
+            //        arrStock[dataCount++] = new Stock(arrToSplit);
+
+
+            //    }
+            //    fileReader.Close();
+            //    fileWriter.Close();
+            //    fromStream.Close();
+            //    toStream.Close();
+
+            //    // print
+            //    Console.WriteLine(title);
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        Console.WriteLine(arrStock[i].getString());
+            //    }
+            //    Console.WriteLine("Data count: " + dataCount);
+            //    //Byte[] buffer = new Byte[500];  //準備暫存的資料陣列
+            //    //            int count;
+            //    //while ((count = fromStream.Read(buffer, 0, buffer.Length)) > 0) //從資料串流讀取資料
+            //    //toStream.Write(buffer, 0, count);   //寫入資料串流
+
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Error: FileCopy '{0}' to '{1}'", sourceFile, targetFile);
+            //}
+
+
+
+
+        }
+        public static Stock[] Resize(Stock[] arr)
+        {
+            Stock[] copy = new Stock[arr.Length * 2];
+            for (int i = 0; i < arr.Length; i++)
+                copy[i] = arr[i];
+            return copy;
+        }
+
+    }
+}
---
+++ 20170905_CSharp_FileManager/20170905_CSharp_FileManager/Trader.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace _CSharp_FileManager
+{
+    public class Trader
+    {
+        // Attrubutes
+        // IO
+        private string sourceFileName;
+        private string destinationFileName;
+        // - in
+        private FileStream fromStream;
+        private StreamReader fileReader;
+        //  - out
+        private FileStream toStream;
+        private StreamWriter fileWriter;
+        // Stock
+        private Stock[] stockList;
+        private int dataCount;
+        private String title;
+
+        // Constructor
+        // 建構子接受欲處理的資料檔路徑
+        public Trader(String sourceFileName)
+        {
+            this.stockList = new Stock[100];
+            this.SetSourceFileName(sourceFileName);
+            this.fromStream = new FileStream(this.sourceFileName, FileMode.Open, FileAccess.Read);
+            this.fileReader = new StreamReader(fromStream,Encoding.UTF8);
+
+            String loadInStr;
+
+            title = fileReader.ReadLine();
+
+            while ((loadInStr = fileReader.ReadLine()) != null)
+            {
+                //Console.WriteLine(loadInStr);
+                //fileWriter.WriteLine(loadInStr);
+                string[] arrToSplit = loadInStr.Split(',');
+
+                if (dataCount == this.stockList.Length)
+                    this.Resize();
+                stockList[dataCount++] = new Stock(arrToSplit);
+            }
+            // Close stream
+            this.fromStream.Close();
+            this.fileReader.Close();
+        }
+
+        // Setter
+        public void SetSourceFileName(string source)
+        {
+            this.sourceFileName = source;
+        }
+        public void SetDestinationFileName(string dest)
+        {
+            this.destinationFileName = dest;
+        }
+        public void Resize()
+        {
+            Stock[] copy = new Stock[this.stockList.Length * 2];
+            for (int i = 0; i < dataCount; i++)
+                copy[i] = this.stockList[i];
+            this.stockList = copy;
+        }
+
+        // Getter
+        // 請提供一個成員方法，接受一個股票名稱與一個檔名作為參數，
+        // 此方法將會從原本的股票交易檔案中篩選出符合指定股票名稱的交易資料，並存入參數所指定的檔案中
+        public bool SearchStock(String stockName, String outputFileName)
+        {
+            string[] select = new string[100];
+            int selectDataCount = 0;
+            for (int i = 0; i < dataCount; i++)
+            {
+                if (stockList[i].SearchByStockCode(stockName) != null)
+                {
+                    if (selectDataCount == select.Length)
+                    {
+                        // Resize
+                        string[] copy = new string[select.Length * 2];
+                        for (int j = 0; j < select.Length; j++)
+                        {
+                            copy[j] = select[j];
+                        }
+                        select = copy;
+                    }
+                    select[selectDataCount++] = stockList[i].GetString();
+                }
+            }
+            // Not found
+            if (selectDataCount == 0)
+                return false;
+
+            // Write in
+            this.toStream = new FileStream(this.destinationFileName, FileMode.Create, FileAccess.Write);
+            this.fileWriter = new StreamWriter(this.toStream);
+
+            try
+            {
+                this.fileWriter.WriteLine(this.title);
+                for (int i = 0; i < selectDataCount; i++)
+                {
+                    this.fileWriter.WriteLine(select[i]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error. File writting issue.");
+            }
+
+            this.fileWriter.Close();
+            this.toStream.Close();
+
+
+            return true;
+        }
+        public string GetStockDataByIndex(int index)
+        {
+            if (index <= dataCount - 1)
+                return stockList[index].GetString();
+            else
+                return null;
+        }
+        public StreamReader GetFileReader()
+        {
+            return this.fileReader;
+        }
+        public String GetSourceFileName()
+        {
+            return this.sourceFileName;
+        }
+    }
+}
