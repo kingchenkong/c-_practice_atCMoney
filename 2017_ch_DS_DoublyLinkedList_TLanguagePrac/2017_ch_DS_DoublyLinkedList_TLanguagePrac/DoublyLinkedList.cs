9ef10c76a4f09531a3b76945c45d0502aae45c57
陳維漢 <wikeyno@gmail.com>
2017-09-21 17:46:45 +0800

    20170921_doublyLinkedList is done , next: Tprogram

---
+++ 2017_ch_DS_DoublyLinkedList_TLanguagePrac/2017_ch_DS_DoublyLinkedList_TLanguagePrac/DoublyLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+namespace _ch_DS_DoublyLinkedList_TLanguagePrac
+{
+    public class DoublyLinkedList
+    {
+        private Node headNode;
+        private Node rearNode;
+        private Node dummyNode;
+
+        //public static string dummy = "Dummy";
+
+        public DoublyLinkedList()
+        {
+            this.dummyNode = new Node("Dummy");
+            this.headNode = dummyNode;
+            this.rearNode = dummyNode;
+        }
+        // Getter
+        public Node GetNode(string key)
+        {
+            // empty list
+            if (this.IsEmpty())
+                return null;
+            // search key's node
+            Node temp = this.rearNode;
+            while (temp != this.dummyNode)
+            {
+                if (temp.GetData() == key)
+                    return temp;
+                else
+                    temp = temp.GetPrevLink();
+            }
+            // key is not found
+            return null;
+        }
+        // Insert
+        public void InsertAtHead(Node newNode)
+        {
+            if (this.headNode == this.dummyNode)
+            {
+                newNode.SetPrevLink(this.dummyNode); // newNode link to dummy
+                this.headNode.SetNextLink(newNode); // dummy link to newNode
+                this.headNode = newNode; // posHead set pos
+                this.rearNode = newNode; // posRear set pos
+            }
+            else
+            {
+                // dummyNode
+                this.dummyNode.SetNextLink(newNode);
+                // newNode
+                newNode.SetPrevLink(this.dummyNode); // newNode link to dummy
+                newNode.SetNextLink(this.headNode); // newNode link to headNode
+                // headNode
+                this.headNode.SetPrevLink(newNode);
+                this.headNode = newNode;
+            }
+        }
+        public void InsertAtRear(Node newNode)
+        {
+            if (this.rearNode == this.dummyNode)
+            {
+                // dummyNode
+                this.dummyNode.SetNextLink(newNode);
+                // newNode
+                newNode.SetPrevLink(this.dummyNode);
+                // position
+                this.headNode = newNode;
+                this.rearNode = newNode;
+            }
+            else
+            {
+                // newNode
+                newNode.SetPrevLink(this.rearNode);
+                newNode.SetNextLink(null); // end
+                // rearNode
+                this.rearNode.SetNextLink(newNode);
+                this.rearNode = newNode;
+            }
+        }
+        // Delete
+        public bool Delete(string key)
+        {
+            // search key node
+            Node deleted = this.GetNode(key);
+            // deleted is not found
+            if (deleted == null)
+                return false;
+            // prev next
+            Node prev = deleted.GetPrevLink();
+            Node next = deleted.GetNextLink();
+            // set link
+            prev.SetNextLink(next);
+            // key != rearNode
+            if (deleted != this.rearNode)
+            {
+                next.SetPrevLink(prev);
+            }
+            else
+            { // this.rearNode == deleted
+                this.rearNode = this.rearNode.GetPrevLink();
+            }
+
+            // check
+            if(this.rearNode == dummyNode)
+            {
+                this.headNode = dummyNode;
+            }
+
+
+            return true;
+        }
+        //
+
+        // Check
+        public bool IsEmpty()
+        {
+            if (this.headNode == this.dummyNode && this.rearNode == this.dummyNode)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 2017_ch_DS_DoublyLinkedList_TLanguagePrac/2017_ch_DS_DoublyLinkedList_TLanguagePrac/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace _ch_DS_DoublyLinkedList_TLanguagePrac
+{
+    public class Node
+    {
+        private string data;
+        private Node prevLink;
+        private Node nextLink;
+
+        public Node(string str)
+        {
+            this.data = str;
+        }
+
+        // Getter
+        public Node GetPrevLink()
+        {
+            return this.prevLink;
+        }
+        public Node GetNextLink()
+        {
+            return this.nextLink;
+        }
+        public string GetData()
+        {
+            return this.data;
+        }
+
+        // Setter
+        public void SetData(string str)
+        {
+            this.data = str;
+        }
+        public void SetPrevLink(Node newPrev){
+            this.prevLink = newPrev;
+        }
+        public void SetNextLink(Node newNext)
+        {
+            this.nextLink = newNext;
+        }
+    }
+}
