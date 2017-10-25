58d7437cad71975c5572fee92e59ad6d50e2a6b2
陳維漢 <wikeyno@gmail.com>
2017-10-25 16:00:49 +0800

    HashTable unFinish

---
+++ 20171024_HashTable/20171024_HashTable/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace _HashTable
+{
+    public class Node
+    {
+        public int data;
+        public Node link;
+        public Node(int d)
+        {
+            this.data = d;
+            this.link = null;
+        }
+    }
+}
---
+++ 20171024_HashTable/20171024_HashTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _HashTable
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("1) Insert. 2) Search. -1) Quit: ");
+                string scanOp = Console.ReadLine();
+                switch (scanOp)
+                {
+                    case "-1":
+                        Console.WriteLine("Bye~");
+                        return;
+                    case "1":
+                        Insert();
+                        break;
+                    case "2":
+                        Search();
+                        break;
+                    default:
+                        Console.WriteLine("Error: scanOp setting error.");
+                        break;
+                }
+            } while (true);
+        }
+        public static void Insert()
+        {
+
+        }
+        public static void Search()
+        {
+
+        }
+    }
+}
